a6245018ac3d9c551a4034f42e1593abbc3a6be6
mairi tikk <mairi.tikk@gmail.com>
2025-06-10 19:03:32 +0200

    add buttons

--- Drimt/src/Components/Hero/Hero.tsx
+++ Drimt/src/Components/Hero/Hero.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import profile_img from '../../assets/profile_mairi.svg'
+import { Button } from '../Navbar/button'
 const Hero = () => {
   return (
     <div className='flex items-center flex-col gap-9'>
@@ -7,8 +8,8 @@ const Hero = () => {
           <h1 className='text-center w-[70%] text-[80px] font-semibold'><span className="text-gradient-logo w-[70%] text-[90px] font-semibold text-center">I'm Mairi Tikk</span>, web developer based in Germnay</h1>
           <p className='w-[50%] text-center text-2xl leading-[40px]'> Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita, tenetur possimus sapiente suscipit eos excepturi explicabo! Cupiditate nisi veritatis, distinctio rerum magnam soluta reprehenderit officiis repellat temporibus facilis, corporis sed.</p>
           <div className="flex items-center gap-25 text-2x1 font-medium mb-[50px]">
-              <div className="hero-connect">Connect with me</div>
-              <div className="hero-ressumen">My resume</div>
+              <Button className=" text-[18px] bg-gradient-to-r from-[#EBEFF5] to-[#3A3AF8] px-10 py-5 rounded-full cursor-pointer">Connect with me</Button>
+              <Button variant="outline" className='text-[18px] rounded-full cursor-pointer px-10 py-5 '>My resume</Button>
     </div>
       </div>
   )
