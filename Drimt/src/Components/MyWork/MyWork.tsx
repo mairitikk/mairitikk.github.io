11046483b626d3cdb67ce0595e522659722976d1
mairi tikk <mairi.tikk@gmail.com>
2025-06-23 11:45:34 +0200

    add img

--- Drimt/src/Components/MyWork/MyWork.tsx
+++ Drimt/src/Components/MyWork/MyWork.tsx
@@ -30,26 +30,15 @@ const MyWork = () => {
      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
         {mywork_data.map((work, index) => (
           <Card key={index}   className='p-6 flex flex-col justify-between h-full bg-blue-100 border-2 border-transparent rounded-lg transition-all duration-400 ease-in-out hover:scale-105 hover:border-[#3A3AF8]  hover:bg-gradient-to-br from-blue-200 to-blue-300 '>
-            <CardHeader className='pb-4'>
-            
-              <CardDescription className='text-xl sm:text-2xl lg:text-3xl font-semibold text-gray-500 mb-2'>
-                {service.s_no}
-              </CardDescription>
-             
-              <CardTitle className='text-3xl sm:text-4xl lg:text-5xl font-bold text-[#3A3AF8]'>
-                {service.s_name}
-              </CardTitle>
-            </CardHeader>
+         
             <CardContent className='flex-grow'>
-              {/* Service Description */}
-              <p className='text-base sm:text-lg lg:text-[24px] font-medium text-gray-700'>
-                {service.s_desc}
-              </p>
+             {/* Image added here */}
+        {work.w_img && <img src={work.w_img} alt={work.s_name || "Work image"} className='w-full h-48 object-cover rounded-md mb-4' />}
             </CardContent>
             <CardFooter className='pt-4 flex justify-end items-center'>
               <Button variant="link" className='text-[#3A3AF8] hover:underline flex items-center space-x-2 p-0 h-auto'>
-  {/* Read More Text */}
-  <p className='text-base sm:text-lg'>Read More</p>
+  {/* Show More Text */}
+  <p className='text-base sm:text-lg'>Show More</p>
   {arrow_icon && <img src={arrow_icon} alt="Arrow Icon" className='w-6 h-6 ml-1' />}
 </Button>
             </CardFooter>
