07647ad435434c0c111255327de741d449a2611d
mairi tikk <mairi.tikk@gmail.com>
2025-06-25 09:16:08 +0200

    add imput

--- Drimt/src/Components/footer/footer.tsx
+++ Drimt/src/Components/footer/footer.tsx
@@ -1,12 +1,20 @@
 import footer_logo from '../../assets/footer_logo.svg'
+import user_icon from '../../assets/user_icon'
+import { Input } from "@/components/ui/input"
 
 const footer = () => {
   return (
     <div className="">
         <div className="footer-top">
             <div className="footer-top-left">
-                <img src="{footer_logo}" alt="footer logo" />
+                <img src={footer_logo} alt="footer logo" />
                 <p>I am an aspiring Full-Stack Developer</p>
+            </div>
+            <div className="footer-top-right">
+                <div className="footer-email-input">
+                    <img src={user_icon} alt="user icon" />
+                    <Input type="email" placeholder="Email" />
+                </div>
             </div>
         </div>
       
