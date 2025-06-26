fbdf8005fcac8b80a54d4a64a55a9c1dc6a267b9
mairi tikk <mairi.tikk@gmail.com>
2025-06-26 13:19:01 +0200

    add router

--- Drimt/src/App.tsx
+++ Drimt/src/App.tsx
@@ -1,24 +1,48 @@
-//import React from 'react'
-import Navbar from './components/navbar/navbar'
-import Hero from './components/hero/hero'
-import About from './components/about/about'
-import Services from './components/services/services'
-import MyWork from './components/mywork/mywork'
-import Contact from './components/contact/contact'
-import Footer from './components/footer/footer'
+
+
+import React from 'react'; 
+import { Routes, Route } from 'react-router-dom'; 
+
+// Import your components
+import Navbar from './components/navbar/navbar';
+import Hero from './components/hero/hero'; 
+import About from './components/about/about';
+import Services from './components/services/services';
+import MyWork from './components/mywork/mywork'; 
+import Contact from './components/contact/contact';
+import Footer from './components/footer/footer';
+
+
+
+const HomePage = () => {
+  return (
+    <>
+      <Hero /></>
+  );
+};
+
 
 const App = () => {
   return (
     <div>
-      <Navbar></Navbar>
-      <Hero></Hero>
-      <About></About>
-      <Services></Services>
-      <MyWork></MyWork>
-      <Contact></Contact>
-      <Footer></Footer>
-    </div>
-  )
-}
+      <Navbar /> 
 
-export default App
+      <main> 
+        <Routes>
+        
+          <Route path="/" element={<HomePage />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/services" element={<Services />} />
+          <Route path="/portfolio" element={<MyWork />} /> 
+          <Route path="/contact" element={<Contact />} />
+
+        
+        </Routes>
+      </main>
+
+      <Footer />
+    </div>
+  );
+};
+
+export default App;
