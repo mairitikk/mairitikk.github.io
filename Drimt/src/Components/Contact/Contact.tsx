bcb71afb620ee906416af9d622cfa96976853448
mairi tikk <mairi.tikk@gmail.com>
2025-06-30 11:04:38 +0200

    onSubmit function changes

--- Drimt/src/Components/Contact/Contact.tsx
+++ Drimt/src/Components/Contact/Contact.tsx
@@ -35,11 +35,27 @@ const Contact = () => {
   });
 
   // Define the onSubmit function
-  const onSubmit = (values: ContactFormValues) => {
-    console.log("Form submitted with values:", values);
+const onSubmit = async (event) => {
+    event.preventDefault();
+    const formData = new FormData(event.target);
 
-    console.log("Form submitted! Check console for values.");
-    form.reset();
+    formData.append("access_key", "YOUR_ACCESS_KEY_HERE");
+
+    const object = Object.fromEntries(formData);
+    const json = JSON.stringify(object);
+
+    const res = await fetch("https://api.web3forms.com/submit", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json"
+      },
+      body: json
+    }).then((res) => res.json());
+
+    if (res.success) {
+      console.log("Success", res);
+    }
   };
   return (
    <section  id="contact" className="flex flex-col items-center justify-center gap-10 lg:gap-20 mt-10 mb-10 mx-5 sm:mx-10 lg:mx-[170px]">
