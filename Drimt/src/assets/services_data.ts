e3efac5789481a99c165e02ed59f59002cdce50d
mairi tikk <mairi.tikk@gmail.com>
2025-07-07 11:28:05 +0200

    services change

--- Drimt/src/assets/services_data.ts
+++ Drimt/src/assets/services_data.ts
@@ -1,34 +1,35 @@
 const Services_Data = [
-    {
-        s_no:"01",
-        s_name:"Web design",
-        s_desc:"Web development is the process of building, programming..."
-    },
-    {
-        s_no:"02",
-        s_name:"Graphics design",
-        s_desc:"Web development is the process of building, programming..."
-    },
-    {
-        s_no:"03",
-        s_name:"Social media",
-        s_desc:"Web development is the process of building, programming..."
-    },
-    {
-        s_no:"04",
-        s_name:"App design",
-        s_desc:"Web development is the process of building, programming..."
-    },
-    {
-        s_no:"05",
-        s_name:"Digital marketing",
-        s_desc:"Web development is the process of building, programming..."
-    },
-    {
-        s_no:"06",
-        s_name:"Content writing",
-        s_desc:"Web development is the process of building, programming..."
-    },
-]
- 
+  {
+    s_no: "01",
+    s_name: "Custom Web Development",
+    s_desc:
+      "Designing, developing, and deploying bespoke web applications from the ground up, tailored to your unique business needs and user requirements.",
+  },
+  {
+    s_no: "02",
+    s_name: "Graphics design",
+    s_desc: "Web development is the process of building, programming...",
+  },
+  {
+    s_no: "03",
+    s_name: "Social media",
+    s_desc: "Web development is the process of building, programming...",
+  },
+  {
+    s_no: "04",
+    s_name: "App design",
+    s_desc: "Web development is the process of building, programming...",
+  },
+  {
+    s_no: "05",
+    s_name: "Digital marketing",
+    s_desc: "Web development is the process of building, programming...",
+  },
+  {
+    s_no: "06",
+    s_name: "Content writing",
+    s_desc: "Web development is the process of building, programming...",
+  },
+];
+
 export default Services_Data;
