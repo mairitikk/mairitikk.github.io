dd999ad1fc3d334776eb7fae40c82aff65b708c1
mairitikk <mairi.tikk@gmail.com>
2026-04-07 13:16:03 +0200

    add two more projects

--- Drimt/src/assets/mywork_data.ts
+++ Drimt/src/assets/mywork_data.ts
@@ -1,6 +1,8 @@
 import project1_img from "../assets/list5.jpg";
 import project2_img from "../assets/drimt_co.png";
 import project3_img from "../assets/pulmad.jpg";
+import project4_img from "../assets/trainingschedule.png";
+import project5_img from "../assets/lens.png";
 
 const mywork_data = [
   {
@@ -19,6 +21,18 @@ const mywork_data = [
     w_no: 3,
     w_name: "Web design",
     w_img: project3_img,
+  },
+  {
+    w_no: 4,
+    w_name: "Training Schedule",
+    w_img: project4_img,
+    w_url: "https://www.trainingschedule.app",
+  },
+  {
+    w_no: 5,
+    w_name: "Lens",
+    w_img: project5_img,
+    w_url: "http://lens.drimt.co",
   },
 ];
 
