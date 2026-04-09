43a4225d83a2a9f1f5c9e960a4c72385ccb301dd
mairitikk <mairi.tikk@gmail.com>
2026-04-09 10:26:37 +0200

    portfolio changes

--- Drimt/src/assets/mywork_data.ts
+++ Drimt/src/assets/mywork_data.ts
@@ -5,34 +5,35 @@ import project4_img from "../assets/trainingschedule.png";
 import project5_img from "../assets/lens.png";
 
 const mywork_data = [
-  {
+ {
     w_no: 1,
-    w_name: "Task Website",
-    w_img: project1_img,
-    w_url: "https://task.drimt.co",
-  },
-  {
-    w_no: 2,
     w_name: "Drimt",
     w_img: project2_img,
     w_url: "https://www.drimt.co/",
   },
   {
-    w_no: 3,
-    w_name: "Web design",
-    w_img: project3_img,
-  },
-  {
-    w_no: 4,
-    w_name: "Training Schedule",
-    w_img: project4_img,
-    w_url: "https://www.trainingschedule.app",
-  },
-  {
-    w_no: 5,
+    w_no: 2,
     w_name: "Lens",
     w_img: project5_img,
     w_url: "http://lens.drimt.co",
+  },
+  {
+    w_no: 3,
+    w_name: "Training Schedule",
+    w_img: project4_img,
+    w_url: "https://www.trainingschedule.app",
+  }, 
+  
+  {
+    w_no: 4,
+    w_name: "Task Website",
+    w_img: project1_img,
+    w_url: "https://task.drimt.co",
+  },
+  {
+    w_no: 5,
+    w_name: "Web design",
+    w_img: project3_img,
   },
 ];
 
